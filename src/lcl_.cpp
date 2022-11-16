4ee77f5e2b96f9762adc22c4260ab9857126d369
UNI-shibuya-taku <s.takuuuuu@gmail.com>
2022-11-16 16:39:53 +0900

    edit

--- src/lcl_.cpp
+++ src/lcl_.cpp
@@ -42,7 +42,9 @@ typedef pcl::PointCloud<PointA> CloudA;
 typedef pcl::PointCloud<PointA>::Ptr CloudAPtr;
 
 CloudAPtr save_pc_ (new CloudA);
+CloudAPtr save_pc_2 (new CloudA);
 CloudAPtr old_pc_ (new CloudA);
+CloudAPtr old_pc_2 (new CloudA);
 
 nav_msgs::Odometry odom_;
 nav_msgs::Odometry init_odom_;
@@ -50,6 +52,7 @@ nav_msgs::Odometry init_odom_;
 nav_msgs::Odometry sq_time;
 
 ros::Publisher pub;
+ros::Publisher pub_2;
 
 ros::Publisher pub_sq_time;
 
@@ -57,11 +60,15 @@ Eigen::Matrix4f transform_matrix;
 Eigen::Matrix4f inverse_transform_matrix;
 
 int count_ = 0;
+int count_2 = 0;
 int save_num;
+int save_num_2;
 
 bool init_lcl_flag = false;
 
-float z_threshold = 30.0;
+// float z_threshold = 30.0;
+double z_threshold;
+double z_threshold_max;
 
 Eigen::Matrix4f create_matrix(nav_msgs::Odometry odom_now, float reflect){
 
@@ -108,14 +115,15 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
                 pow(single_pc_->points[i].y, 2)+
                 pow(single_pc_->points[i].z, 2));
         if(distance < 50){	//調整可
-            //if(single_pc_->points[i].z <= z_threshold){
-            PointA temp;
-            output_pc_after->points.push_back(output_pc->points[i]);
+            if(single_pc_->points[i].z <= z_threshold){
+                PointA temp;
+                output_pc_after->points.push_back(output_pc->points[i]);
+            }
         }
     }
 
     // output_pc_after -> output_save_pc
-    CloudAPtr output_save_pc (new CloudA);
+    CloudAPtr output_save_pc (new CloudA); // 重ねるスキャンライン
     Eigen::Matrix4f inverse_transform_matrix = transform_matrix.inverse();
     pcl::transformPointCloud(*output_pc_after, *output_save_pc, inverse_transform_matrix);
 
@@ -129,7 +137,6 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
         *save_pc_ += *output_save_pc;
         old_pc_ = output_save_pc;
     }
-
     save_pc_->header = single_pc_->header;
     save_pc_->width = save_pc_->points.size();
     save_pc_->height = 1;
@@ -137,8 +144,26 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
     sensor_msgs::PointCloud2 pc_;
     pcl::toROSMsg(*save_pc_, pc_);
     pub.publish(pc_);
+    std::cout << "save_pc_ size : " << save_pc_->points.size() << std::endl;
+
+    // if(count_2 < save_num_2){
+    //     *save_pc_2 += *output_save_pc;
+    // }else{
+    //     int old_pc_size_2 = (int)old_pc_2->points.size();
+    //     save_pc_2->points.erase(save_pc_2->points.begin(), save_pc_2->points.begin()+old_pc_size_2);
+    //     *save_pc_2 += *output_save_pc;
+    //     old_pc_2 = output_save_pc;
+    // }
+    // save_pc_2->header = single_pc_->header;
+    // save_pc_2->width = save_pc_2->points.size();
+    // save_pc_2->height = 1;
+    // // save_pc_->header.stamp = ros::Time::now();
+    // sensor_msgs::PointCloud2 pc_2;
+    // pcl::toROSMsg(*save_pc_2, pc_2);
+    // pub_2.publish(pc_2);
+    // std::cout << "save_pc_2 size: " << save_pc_2->points.size() << std::endl;
+
     // pub.publish(save_pc_);
-    std::cout << "save_pc_ size: " << save_pc_->points.size() << std::endl;
     // pc_.header.stamp = ros::Time::now();
     // pc_.header.frame_id = msg->header.frame_id;
     // pc_.header.frame_id = "/odom3d";	//変更
@@ -146,6 +171,7 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
     // cout<<"cloud_lcl header" << pc_.header <<endl;
 
     count_++;
+    count_2++;
 }
 
 int main(int argc, char** argv)
@@ -154,10 +180,14 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
 	ros::NodeHandle nh("~");
 	nh.getParam("save_num", save_num);
+	nh.getParam("save_num_2", save_num_2);
+	nh.param("z_threshold", z_threshold, {100.0});
+	nh.param("z_threshold_max", z_threshold_max, {-100.0});
 
     ros::Subscriber sub_pc = n.subscribe("/cloud", 30, pc_callback); // cloud/tf
     ros::Subscriber sub_lcl = n.subscribe("/odom", 30, lcl_callback);
     pub = n.advertise<sensor_msgs::PointCloud2>("/cloud/lcl", 30);
+    pub_2 = n.advertise<sensor_msgs::PointCloud2>("/cloud/lcl/second", 30);
 
     nav_msgs::Odometry init_odom;
     init_odom.header.frame_id = "/map";
